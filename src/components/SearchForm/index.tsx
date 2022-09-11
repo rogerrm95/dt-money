f03d5d4bc183cdf7a9d3869d0ede488e42c3353b
rogerrm95 <rogerrm@live.com>
2022-09-11 20:22:05 -0300

    add: Performance - understanding and aplly memo

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -7,6 +7,7 @@ import { MagnifyingGlass } from 'phosphor-react'
 // Styles //
 import { SearchFormContainer } from './styles'
 import { useTransaction } from '../../hooks/useTransaction'
+import { memo } from 'react'
 
 // SCHEMA //
 const searchFormSchema = z.object({
@@ -15,7 +16,7 @@ const searchFormSchema = z.object({
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
-export function SearchForm() {
+function SearchFormComponent() {
   const { fetchTransactions } = useTransaction()
   const {
     register,
@@ -44,3 +45,23 @@ export function SearchForm() {
     </SearchFormContainer>
   )
 }
+
+export const SearchForm = memo(SearchFormComponent)
+
+/** Sobre o uso do MEMO
+ *
+ * Por que que um componente renderiza ?
+ * - Hooks changed (mudou estado, contexto, reducer);
+ * - Props changed (mudou propriedades);
+ * - Parent rerendered (componente pai renderizou);
+ *
+ * Qual o fluxo de renderização ?
+ * 1. O React recria o HTML da interface daquele component
+ * 2. Compara a versão do HTML recriada com a versão anterior
+ * 3. SE mudou alguma coisa, ele reescreve o HTML na tela
+ *
+ * Memo: Adiciona um passo a mais antes do fluxo acima
+ * 0. Hooks changed, Props changed (deep comparison)
+ * 0.1 Comparar com a versão anterior dos hooks e props
+ * 0.2 SE mudou algo, ele irá permitir a nova renderização...
+ */
